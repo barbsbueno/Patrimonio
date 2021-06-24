51dc2b97230928cc3737cd1a12e9a4d2c301d4a0
Danilo <79422857+danilofalcirolli@users.noreply.github.com>
2021-06-24 10:02:56 -0300

    comitDanilo

--- PatrimonioJava/src/domain/ItemPatrimonio.java
+++ PatrimonioJava/src/domain/ItemPatrimonio.java
@@ -1,5 +1,101 @@
 package domain;
 
 public class ItemPatrimonio {
+	private int id;
+	private String data;
+	private String equipamento;
+	private double valor;
+	
+	ItemPatrimonio(){
+		
+	}
+	
+	public ItemPatrimonio(int id, String data, String equipamento, double valor) {
+		this.id = id;
+		this.data = data;
+		this.equipamento = equipamento;
+		this.valor = valor;
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getEquipamento() {
+		return equipamento;
+	}
+
+	public void setEquipamento(String equipamento) {
+		this.equipamento = equipamento;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public String toString() {
+		return "ItemPatrimonio [id=" + id + ", data=" + data + ", equipamento=" + equipamento + ", valor=" + valor
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((data == null) ? 0 : data.hashCode());
+		result = prime * result + ((equipamento == null) ? 0 : equipamento.hashCode());
+		result = prime * result + id;
+		long temp;
+		temp = Double.doubleToLongBits(valor);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ItemPatrimonio other = (ItemPatrimonio) obj;
+		if (data == null) {
+			if (other.data != null)
+				return false;
+		} else if (!data.equals(other.data))
+			return false;
+		if (equipamento == null) {
+			if (other.equipamento != null)
+				return false;
+		} else if (!equipamento.equals(other.equipamento))
+			return false;
+		if (id != other.id)
+			return false;
+		if (Double.doubleToLongBits(valor) != Double.doubleToLongBits(other.valor))
+			return false;
+		return true;
+	}
+	
+	
+	
+	
+	
+	
 }
